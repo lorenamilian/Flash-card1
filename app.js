6bef85655719c32c99f6979363f7cb98e1dfdcfb
lorena <lorenalies2312@gmail.com>
2019-12-07 21:49:19 -0500

    wfgh

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/static', express.static('public'));
+app.use("/static", express.static("public"));
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-const mainRoutes = require('./routes');
-const cardRoutes = require('./routes/cards');
+const mainRoutes = require("./routes");
+const cardRoutes = require("./routes/cards");
 
 app.use(mainRoutes);
-app.use('/cards', cardRoutes);
+app.use("/cards", cardRoutes);
 
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -25,9 +25,9 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
-  res.render('error');
+  res.render("error");
 });
 
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!')
+app.listen(process.env.PORT || 3000, () => {
+  console.log("The application is running on localhost:3000!");
 });
